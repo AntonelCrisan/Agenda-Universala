983af8d5eb19230967ac730e40a131ffbd609e95
Antonel Gabriel Crișan <antonel19.gabriel@gmail.com>
2023-11-13 10:07:04 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-
+        Random agende = new Random();
+        String[] aplicatii = {"Twos", "Obsidian", "Joplin", "BearApp"};
+        for(int i = 0; i <= 3; i++){
+                int random = agende.nextInt(3);
+                System.out.println(random);
+        }
 
     }
 }
